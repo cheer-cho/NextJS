26eb588941d59a8eb8d15d420a460996b4220936
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-26 13:31:10 -0600

    [2] - Routing - [9] - Private Folders

---
+++ 2-routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+// You can include this _lib folder in the route if change from _ to %5F prefix
+
+export default function PrivateRoute() {
+  return <h1>You cannot view this page in browser</h1>;
+}
