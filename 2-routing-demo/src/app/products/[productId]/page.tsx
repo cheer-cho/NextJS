e8ed1915bd52558aaa77736e53fcb49f415c15f5
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-26 15:29:08 -0600

    [4] - Metadata - [2] - Dynamic metadata

--- 2-routing-demo/src/app/products/[productId]/page.tsx
+++ 2-routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-interface ProductDetailsProps {
+import { Metadata } from 'next';
+
+type ProductDetailsProps = {
   params: {
     productId: string;
   };
-}
+};
+
+export const generateMetadata = async ({
+  params: { productId },
+}: ProductDetailsProps): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
 
 const ProductDetails = ({ params }: ProductDetailsProps) => {
   return <h1>Details about product {params.productId} </h1>;
