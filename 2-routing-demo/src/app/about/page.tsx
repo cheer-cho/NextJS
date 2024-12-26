1933cf61d07728ccec7ea240e9ce315c45c8e920
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-26 15:22:33 -0600

    [4] - Metadata - [1] - Static metadata

--- 2-routing-demo/src/app/about/page.tsx
+++ 2-routing-demo/src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'About me',
+  description: 'About me',
+};
+
 const About = () => {
   return <h1>About me</h1>;
 };
--- 2-routing-demo/src/app/layout.tsx
+++ 2-routing-demo/src/app/layout.tsx
@@ -3,7 +3,11 @@ export const metadata = {
   description: 'Study Next.js',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='en'>
       <body>
