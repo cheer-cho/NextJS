e107047291161a44cd7e4cda09fc7e2c6de4a006
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-30 12:35:46 -0600

    [8] - Error Page - [2] - Recoverring from Error using reset()

--- 2-routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ 2-routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
 export default function Error({
   error,
+  reset,
 }: {
   error: Error & { digest?: string };
+  reset: () => void;
 }) {
-  return <div>{error.message}</div>;
+  const router = useRouter();
+
+  const refreshAndReset = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  return (
+    <div>
+      {error.message} <button onClick={refreshAndReset}>Try again</button>
+    </div>
+  );
 }
