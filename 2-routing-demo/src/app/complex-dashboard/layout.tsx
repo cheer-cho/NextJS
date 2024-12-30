202b1499d857993131f936f0c835ff687eb2e94c
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-30 15:53:32 -0600

    [2] - Routing (Continue) - [13] - conditional renderring routes

--- 2-routing-demo/src/app/complex-dashboard/layout.tsx
+++ 2-routing-demo/src/app/complex-dashboard/layout.tsx
@@ -5,13 +5,17 @@ export default function DashboardLayout({
   users,
   revenues,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenues: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = false;
+
+  return isLoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: 'flex' }}>
@@ -22,5 +26,7 @@ export default function DashboardLayout({
         <div style={{ display: 'flex', flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
