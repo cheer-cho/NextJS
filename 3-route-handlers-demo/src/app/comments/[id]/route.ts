62c4f9ac2562f829ff1bb22e6ee43bda5f7647c7
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-31 16:54:46 -0600

    [Demo-3 - CRUD] - [1] - Route Handler - [5] - handle PATCH request

--- 3-route-handlers-demo/src/app/comments/[id]/route.ts
+++ 3-route-handlers-demo/src/app/comments/[id]/route.ts
@@ -7,3 +7,21 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
 
   return Response.json(comment, { status: 200 });
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
+  if (index === -1) {
+    return Response.json({ message: 'Comment not found' }, { status: 404 });
+  }
+
+  const updatedComment = await request.json();
+  comments[index].text = updatedComment.text;
+
+  return Response.json(comments[index], { status: 200 });
+}
