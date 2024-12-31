20058ba5a9885245cdb37ff992f96ff7ca693b4f
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2024-12-31 17:13:23 -0600

    [Demo-3 - CRUD] - [1] - Route Handler - [6] - URL Query Parameters

--- 3-route-handlers-demo/src/app/comments/[id]/route.ts
+++ 3-route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,9 +1,8 @@
 import { comments } from './../data';
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const { id } = await params;
+  const comment = comments.find((comment) => comment.id === parseInt(id));
 
   return Response.json(comment, { status: 200 });
 }
@@ -12,9 +11,8 @@ export async function PATCH(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
 
   if (index === -1) {
     return Response.json({ message: 'Comment not found' }, { status: 404 });
@@ -24,4 +22,20 @@ export async function PATCH(
   comments[index].text = updatedComment.text;
 
   return Response.json(comments[index], { status: 200 });
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+
+  if (index === -1) {
+    return Response.json({ message: 'Comment not found' }, { status: 404 });
+  }
+
+  const deletedComment = comments.splice(index, 1);
+
+  return Response.json(deletedComment[0], { status: 200 });
 }
--- 3-route-handlers-demo/src/app/comments/route.ts
+++ 3-route-handlers-demo/src/app/comments/route.ts
@@ -1,7 +1,15 @@
+import { NextRequest } from 'next/server';
 import { comments } from './data';
 
-export async function GET() {
-  return Response.json(comments, { status: 200 });
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const query = searchParams.get('query');
+
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json(filteredComments, { status: 200 });
 }
 
 export async function POST(request: Request) {
