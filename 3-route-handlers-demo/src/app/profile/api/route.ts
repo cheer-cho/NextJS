5b9233382ba092f872b360cc2d7f884d5696e8aa
Cheer Chokwittaya <178830338+cheer-cho@users.noreply.github.com>
2025-01-04 18:38:23 -0600

    [Demo-3 - CRUD] - [1] - Route Handler - [8] - Headers in Route Handlers

--- 3-route-handlers-demo/src/app/profile/api/route.ts
+++ 3-route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response('Profile data', { status: 200 });
+import { type NextRequest } from 'next/server';
+import { headers } from 'next/headers';
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  const headerList = await headers();
+
+  console.log(headerList.get('authorization'));
+  console.log(requestHeaders.get('authorization'));
+  return new Response('<h1>Profile data</h1>', {
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
 }
